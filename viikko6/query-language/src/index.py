d6a1bbc57e6a42837c38e8de7e85087d9ce28401
Uxusino Uxusone <89293277+Uxusino@users.noreply.github.com>
2024-12-03 19:44:32 +0200

    Viikko 6, Tehtävä 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,20 +1,21 @@
 from player_statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All
+from matchers import And, HasAtLeast, PlaysIn, Not, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
-
-
     matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
+
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,19 +9,30 @@ class And:
 
         return True
     
-class All:
-    def __init__(self):
-        pass
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
 
     def test(self, player):
-        return True
-    
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+            
+        return False
+
 class Not:
     def __init__(self, matcher):
         self._matcher = matcher
 
     def test(self, player):
         return not self._matcher.test(player)
+    
+class All:
+    def __init__(self):
+        pass
+
+    def test(self, player):
+        return True
 
 class PlaysIn:
     def __init__(self, team):
